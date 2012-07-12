e5309297d4649fd81cd08b916b52def96e99dafe
Pawel Pierzchala <pawel.pierzchala@llp.pl>
2012-07-12 18:33:34 +0200

    Fast refactor #18

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -55,7 +55,7 @@ module UglyTrivia
       puts "They have rolled a #{roll}"
 
       if in_penalty_box?
-        if roll % 2 != 0
+        if roll.odd?
           @is_getting_out_of_penalty_box = true
 
           puts "#{current_player} is getting out of the penalty box"
